389bd853de5d591a0e02ef94dcb701a77a019492
Jahidultr <jahidulislamdiu02@gmail.com>
2024-06-27 09:40:48 -0600

    update

---
+++ constant.dart
@@ -0,0 +1,10 @@
+main() {
+  const String university = 'Dhaka International university';
+  const int age = 345;
+  const double pi = 3.1416;
+
+  final double gpa;
+  gpa = 3.65;
+  print(university);
+  print(gpa);
+}
